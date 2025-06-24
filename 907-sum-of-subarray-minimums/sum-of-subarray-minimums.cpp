295250c274a1fef3a6847152e4bc83e29b20b495
avishjhalani <avishjhalani20@gmail.com>
2025-06-24 17:14:06 +0000

    Time: 16 ms (91.34%), Space: 47.3 MB (87.99%) - LeetSync

---
+++ 907-sum-of-subarray-minimums/sum-of-subarray-minimums.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int sumSubarrayMins(vector<int>& arr) {
+        const int MOD = 1e9 + 7;
+        int n = arr.size();
+        vector<int> left(n), right(n);
+        stack<int> st;
+
+        for (int i = 0; i < n; ++i) {
+            while (!st.empty() && arr[st.top()] > arr[i])
+                st.pop();
+            left[i] = st.empty() ? i + 1 : i - st.top();
+            st.push(i);
+        }
+
+        st = stack<int>();
+
+        for (int i = n - 1; i >= 0; --i) {
+            while (!st.empty() && arr[st.top()] >= arr[i])
+                st.pop();
+            right[i] = st.empty() ? n - i : st.top() - i;
+            st.push(i);
+        }
+
+         long long res = 0;
+        for (int i = 0; i < n; ++i) {
+            res = (res + (long long)arr[i] * left[i] * right[i]) % MOD;
+        }
+
+        return (int)res;
+    }
+};
