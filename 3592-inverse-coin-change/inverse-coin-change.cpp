82f8cbff635f6c3bccf827f54a8b26cd37d931df
avishjhalani <avishjhalani20@gmail.com>
2026-07-11 17:51:42 +0000

    Time: 0 ms (100.00%), Space: 26.5 MB (96.88%) - LeetSync

---
+++ 3592-inverse-coin-change/inverse-coin-change.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+        vector<int> findCoins(vector<int>& dp) {
+        int n = dp.size();
+        dp.insert(dp.begin(), 1); // prepend dp[0] = 1
+        vector<int> res;
+        for (int a = 1; a <= n; ++a) {
+            if (dp[a] > 1) return {};
+            if (dp[a] == 0) continue;
+            res.push_back(a);
+            for (int v = n; v >= a; --v) {
+                dp[v] -= dp[v - a];
+                if (dp[v] < 0) return {};
+            }
+        }
+        return res;
+    }
+};
