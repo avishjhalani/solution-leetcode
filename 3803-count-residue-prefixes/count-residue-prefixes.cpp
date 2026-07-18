1d457e1eb455c7f16d697ace4116a8097ab0cd7b
avishjhalani <avishjhalani20@gmail.com>
2026-07-18 15:51:54 +0000

    Time: 7 ms (11.01%), Space: 10.1 MB (38.77%) - LeetSync

---
+++ 3803-count-residue-prefixes/count-residue-prefixes.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int residuePrefixes(string s) {
+        int n = s.size();
+        set<char>st;
+        int ans =0;
+        for(int i =0;i<n;i++){
+            st.insert(s[i]);
+            if(st.size()==(i+1)%3) ans++;
+        }
+        return ans;
+    }
+};
