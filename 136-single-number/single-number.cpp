e7afbdd1dd7789397d18f20968959e9a60c6dbc0
avishjhalani <avishjhalani20@gmail.com>
2026-07-21 07:29:06 +0000

    Time: 0 ms (100.00%), Space: 20.7 MB (59.17%) - LeetSync

---
+++ 136-single-number/single-number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int n = nums.size();
+        if(n==1) return nums[0];
+        int ans =0;
+        for(int i : nums){
+            ans =ans^i;
+        }
+        return ans;
+    }
+};
