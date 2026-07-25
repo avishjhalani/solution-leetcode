4cc0efd3690a615d278ea2f95629055d0eb90763
avishjhalani <avishjhalani20@gmail.com>
2026-07-25 10:41:45 +0000

    Time: 0 ms (100.00%), Space: 12.5 MB (16.08%) - LeetSync

---
+++ 3536-maximum-product-of-two-digits/maximum-product-of-two-digits.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def maxProduct(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        
+        m= sorted(str(n))
+        return int(m[-1])*int (m[-2])
