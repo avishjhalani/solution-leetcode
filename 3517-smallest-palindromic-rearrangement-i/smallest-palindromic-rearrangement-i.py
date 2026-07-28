74c1630de4f47ba772326cbe918f0b5691631047
avishjhalani <avishjhalani20@gmail.com>
2026-07-28 16:16:46 +0000

    Time: 340 ms (82.35%), Space: 13.4 MB (76.47%) - LeetSync

--- 3517-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.py
+++ 3517-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.py
@@ -1,24 +1,17 @@
 class Solution(object):
     def smallestPalindrome(self, s):
+        store = defaultdict(int)
+        res = ''
+        singles = ''
+        for i in s:
+            store[i]+=1
+        for letter in string.ascii_lowercase:
+            res+=letter*(store[letter]//2)
+            if store[letter]%2==1:
+                singles+=letter
+        return res+singles+res[::-1]
         """
         :type s: str
         :rtype: str
         """
-        n = len(s)
-        count ={}
-
-        for c in s:
-            index = ord(c) - ord('a')
-            count[index] = count.get(index, 0) + 1
-        left =""
-        mid =""
-        for i in range (0,26):
-            freq = count.get(i,0)
-
-            if freq%2 == 1:
-                mid += chr(ord('a')+i)
-            left +=  chr(ord('a') + i) * (count.get(i, 0) // 2)
         
-        right = left[::-1]
-
-        return left+mid+right
