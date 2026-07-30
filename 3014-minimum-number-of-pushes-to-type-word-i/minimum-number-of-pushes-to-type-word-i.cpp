5cc51060fe7854ec266f2d711ff1db76d92f4e82
avishjhalani <avishjhalani20@gmail.com>
2026-07-30 08:36:43 +0000

    Time: 0 ms (100.00%), Space: 8.5 MB (31.81%) - LeetSync

--- 3014-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.cpp
+++ 3014-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.cpp
@@ -4,15 +4,15 @@ public:
         int n = word.size();
         if(n<=8) return n;
         int result =0;
-        unordered_map<int,int>mp;
-        int assign =2;
-        for(char &ch :word){
-            if(assign>9){
-                assign =2;
-            }
-            mp[assign]++;
-            result+=mp[assign];
-            assign++;
+        vector<int>freq(26,0);
+        for(char &ch : word){
+            freq[ch-'a']++;
+        }
+        sort(freq.begin(),freq.end(),greater<int>());
+        for(int i =0;i<26;i++){
+            int temp = freq[i];
+            int press = i/8+1;
+            result+=press*temp;
         }
         return result;
     }
