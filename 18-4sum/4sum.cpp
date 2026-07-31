917589a5fa57686c4429e16491f26e7265c66734
avishjhalani <avishjhalani20@gmail.com>
2026-07-31 14:42:24 +0000

    Time: 23 ms (53.95%), Space: 17.4 MB (56.22%) - LeetSync

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -1,48 +1,33 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        vector<vector<int>>ans;
         int n = nums.size();
-        sort(nums.begin(), nums.end());
-
-        vector<vector<int>> ans;
-
-        for(int i = 0; i < n; i++) {
-            if(i > 0 && nums[i] == nums[i - 1]) continue;
-
-            for(int j = i + 1; j < n; j++) {
-                if(j > i + 1 && nums[j] == nums[j - 1]) continue;
-
-                int k = j + 1;
-                int l = n - 1;
-
-                while(k < l) {
-                    long long sum =
-                        (long long)nums[i] +
-                        nums[j] +
-                        nums[k] +
-                        nums[l];
-
-                    if(sum == target) {
-                        ans.push_back(
-                            {nums[i], nums[j], nums[k], nums[l]}
-                        );
-
-                        k++;
-                        l--;
-
-                        while(k < l && nums[k] == nums[k - 1]) k++;
-                        while(k < l && nums[l] == nums[l + 1]) l--;
+        sort(nums.begin(),nums.end());
+        for(int i =0;i<n;i++){
+            if(i>0 &&nums[i]==nums[i-1]) continue;
+            for(int j=i+1;j<n;j++){
+                if(j>i+1 && nums[j]==nums[j-1]) continue;
+                int l =j+1;
+                int r =n-1;
+                while(l<r){
+                    long long  sum =1LL* nums[i]+nums[j]+nums[l]+nums[r];
+                    if(sum == target){
+                        ans.push_back({nums[i],nums[j],nums[l],nums[r]});
+                        l++;
+                        r--;
+                        while(l<r && nums[l]==nums[l-1]){
+                            l++;
+                        }
+                        while(l<r && nums[r]==nums[r+1]){
+                            r--;
+                        }
                     }
-                    else if(sum < target) {
-                        k++;
-                    }
-                    else {
-                        l--;
-                    }
+                    else if(sum<target) l++;
+                    else r--;
                 }
             }
         }
-
         return ans;
     }
 };
