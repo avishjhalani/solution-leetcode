31cd4e0773ec88e46bce8ea28114951c0bbc2c2f
avishjhalani <avishjhalani20@gmail.com>
2026-08-03 10:14:36 +0000

    Time: 8 ms (94.81%), Space: 136.1 MB (83.46%) - LeetSync

--- 1406-stone-game-iii/stone-game-iii.cpp
+++ 1406-stone-game-iii/stone-game-iii.cpp
@@ -1,36 +1,20 @@
 class Solution {
 public:
-int n;
-vector<int>t;
-int solve(int n , int i,vector<int>& nums){
-    if(i>=n){
-        return 0;
-    }
-    if(t[i]!=-1){
-        return t[i];
-    }
-    int result =nums[i] -solve(n,i+1,nums);
-    if(i+1<n){
-        result =max(result, nums[i]+nums[i+1]-solve(n,i+2,nums));
-    }
-    if(i+2<n){
-        result = max(result,nums[i]+nums[i+1]+nums[i+2]-solve(n,i+3,nums));
-    }
-    return t[i]=result;
-
-}
     string stoneGameIII(vector<int>& nums) {
-      n = nums.size();
-      t.resize(n+1,-1);
-      int ans =solve(n,0,nums);
-      if(ans>0){
-        return "Alice";
-      }
-      else if(ans == 0){
+        int n = nums.size();
+        vector<int>t(n+1);
+        for(int i =n-1;i>=0;i--){
+            t[i] = nums[i]-t[i+1];
+            if(i+1<n){
+                t[i] =max(t[i],nums[i]+nums[i+1]-t[i+2]);
+            }
+            if(i+2<n){
+                t[i] =max(t[i],nums[i]+nums[i+1]+nums[i+2]-t[i+3]);
+            }
+            
+        }
+        if(t[0]>0)return "Alice";
+        else if(t[0]<0) return "Bob";
         return "Tie";
-      }
-      else{
-        return "Bob";
-      }
     }
 };
