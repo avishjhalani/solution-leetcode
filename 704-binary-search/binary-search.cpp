6876e25f7dd46fb386c01be01381f56f4ff0b80b
avishjhalani <avishjhalani20@gmail.com>
2026-08-05 17:24:10 +0000

    Time: 0 ms (100.00%), Space: 31.4 MB (47.06%) - LeetSync

--- 704-binary-search/binary-search.cpp
+++ 704-binary-search/binary-search.cpp
@@ -7,9 +7,9 @@ public:
         while(low<=high){
             int mid = low+(high-low)/2;
             if(nums[mid]==target) return mid;
-            else if(nums[mid]>target) high = mid-1;
+            else if(nums[mid]<target) low = mid+1;
             else{
-                low = mid+1;
+                high = mid-1;
             }
         }
         return -1;
