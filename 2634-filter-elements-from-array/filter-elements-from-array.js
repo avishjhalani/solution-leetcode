c33d3e1ca6606c5ecf79470302d4af79e5601e61
avishjhalani <avishjhalani20@gmail.com>
2026-08-06 10:05:42 +0000

    Time: 48 ms (31.73%), Space: 52.4 MB (95.52%) - LeetSync

--- 2634-filter-elements-from-array/filter-elements-from-array.js
+++ 2634-filter-elements-from-array/filter-elements-from-array.js
@@ -4,7 +4,7 @@
  * @return {number[]}
  */
 var filter = function(arr, fn) {
-    let filteredArr=[];
+    const filteredArr=[];
     for(let i =0;i<arr.length;i++){
         if(fn(arr[i],i)){
             filteredArr.push(arr[i]);
