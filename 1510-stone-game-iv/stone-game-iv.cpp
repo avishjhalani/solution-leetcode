2b54f02427c991034a7cd2cad580d0fe2139fed7
avishjhalani <avishjhalani20@gmail.com>
2026-08-10 18:34:47 +0000

    Time: 171 ms (17.75%), Space: 16.9 MB (39.10%) - LeetSync

---
+++ 1510-stone-game-iv/stone-game-iv.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+vector<int>t;
+bool solve(int n){
+    if(n==0) return false;
+    if(t[n]!=-1){
+        return t[n]==1?true:false;;
+    }
+    for(int i =1;i*i<=n;i++){
+        if(solve(n-i*i)==false) return t[n]=true;
+    }
+    return false;
+}
+    bool winnerSquareGame(int n) {
+        t.resize(n+1,-1);
+        return solve(n);
+    }
+};
