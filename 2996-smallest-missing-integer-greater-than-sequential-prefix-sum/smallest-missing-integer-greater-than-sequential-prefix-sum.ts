8db90d1b08adfae26a4891be5f25868e161b8ca9
avishjhalani <avishjhalani20@gmail.com>
2026-08-11 07:46:27 +0000

    Time: 0 ms (100.00%), Space: 57.3 MB (64.00%) - LeetSync

--- 2996-smallest-missing-integer-greater-than-sequential-prefix-sum/smallest-missing-integer-greater-than-sequential-prefix-sum.ts
+++ 2996-smallest-missing-integer-greater-than-sequential-prefix-sum/smallest-missing-integer-greater-than-sequential-prefix-sum.ts
@@ -1,6 +1,6 @@
 function missingInteger(nums: number[]): number {
     const n = nums.length;
-    let st:Set<number>=new Set(nums);
+    const st:Set<number>=new Set(nums);
     let sum =nums[0];
     for(let i=1;i<n;i++){
         if(nums[i]==nums[i-1]+1){
