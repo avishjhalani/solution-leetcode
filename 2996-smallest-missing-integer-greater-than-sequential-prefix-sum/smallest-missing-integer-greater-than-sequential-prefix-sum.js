e4e697b0fb5ddf9e50f00bab9e9f0aad3afd4f67
avishjhalani <avishjhalani20@gmail.com>
2026-08-11 07:42:25 +0000

    Time: 0 ms (100.00%), Space: 55.6 MB (41.30%) - LeetSync

---
+++ 2996-smallest-missing-integer-greater-than-sequential-prefix-sum/smallest-missing-integer-greater-than-sequential-prefix-sum.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingInteger = function(nums) {
+    const n =nums.length;
+    let st =new Set(nums);
+    let sum=nums[0];
+    for(let i=1;i<n;i++){
+        if(nums[i]==nums[i-1]+1){
+            sum+=nums[i];
+        }
+        else{
+            break;
+        }
+    }
+    while(st.has(sum)){
+        sum+=1;
+    }
+    return sum;
+};
