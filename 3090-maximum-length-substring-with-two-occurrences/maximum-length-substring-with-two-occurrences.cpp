122a8b9b5b01da4d88fd8b951cb109de4e775a2d
avishjhalani <avishjhalani20@gmail.com>
2026-08-14 06:30:18 +0000

    Time: 3 ms (42.93%), Space: 9.4 MB (67.36%) - LeetSync

--- 3090-maximum-length-substring-with-two-occurrences/maximum-length-substring-with-two-occurrences.cpp
+++ 3090-maximum-length-substring-with-two-occurrences/maximum-length-substring-with-two-occurrences.cpp
@@ -4,14 +4,13 @@ public:
         int n = s.size();
         int i=0,j=0,count=0;
         unordered_map<int,int>mp;
-        while(j<n){
+        for(j=0;j<n;j++){
             mp[s[j]]++;
             while(mp[s[j]]>2){
                 mp[s[i]]--;
                 i++;
             }
             count =max(count,j-i+1);
-            j++;
         }
         return count;
     }
