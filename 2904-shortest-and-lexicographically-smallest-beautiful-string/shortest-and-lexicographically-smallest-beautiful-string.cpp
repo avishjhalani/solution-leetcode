7cd64064aea632922c5371e948b2c379ec1c09a3
avishjhalani <avishjhalani20@gmail.com>
2026-08-26 08:39:48 +0000

    Time: 0 ms (100.00%), Space: 8.7 MB (82.87%) - LeetSync

--- 2904-shortest-and-lexicographically-smallest-beautiful-string/shortest-and-lexicographically-smallest-beautiful-string.cpp
+++ 2904-shortest-and-lexicographically-smallest-beautiful-string/shortest-and-lexicographically-smallest-beautiful-string.cpp
@@ -2,45 +2,31 @@ class Solution {
 public:
     string shortestBeautifulSubstring(string s, int k) {
         int n = s.size();
-        int count1 = 0;
         int left = 0;
-
+        int count = 0;
         string ans = "";
-
         for (int right = 0; right < n; right++) {
-
-            // Add current element
-            if (s[right] == '1')
-                count1++;
-
-            // We only need windows containing exactly k ones
-            while (count1 > k) {
-                if (s[left] == '1')
-                    count1--;
-
+            if (s[right] == '1') {
+                count++;
+            }
+            while (count > k) {
+                if (s[left] == '1') {
+                    count--;
+                }
                 left++;
             }
-
-            // We have exactly k ones
-            if (count1 == k) {
-
-                // Remove unnecessary leading zeros
+            if (count == k) {
                 while (left < right && s[left] == '0') {
                     left++;
                 }
-
-                string curr = s.substr(left, right - left + 1);
-
-                // Shorter is better.
-                // If same length, lexicographically smaller is better.
-                if (ans == "" ||
-                    curr.size() < ans.size() ||
-                    (curr.size() == ans.size() && curr < ans)) {
-                    ans = curr;
-                }
+            
+            string curr = s.substr(left, right - left + 1);
+            if (ans == "" || curr.size() < ans.size() ||
+                (curr.size() == ans.size() && curr < ans)) {
+                ans = curr;
             }
         }
-
+        }
         return ans;
     }
 };
