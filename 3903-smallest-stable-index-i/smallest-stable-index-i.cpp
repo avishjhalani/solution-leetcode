2f1fb5020de93876305e536335fbd380b53dd278
avishjhalani <avishjhalani20@gmail.com>
2026-09-04 07:19:59 +0000

    Time: 4 ms (13.74%), Space: 30.2 MB (94.42%) - LeetSync

---
+++ 3903-smallest-stable-index-i/smallest-stable-index-i.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int firstStableIndex(vector<int>& nums, int k) {
+        int n = nums.size();
+        for(int i =0;i<n;i++){
+            int minele = *min_element(nums.begin()+i,nums.end());
+            int maxele =*max_element(nums.begin(),nums.begin()+i+1);
+            if((maxele-minele)<=k){
+                return i;
+            }
+        }
+        return -1;
+    }
+};
